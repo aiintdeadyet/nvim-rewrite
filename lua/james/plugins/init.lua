baa16b6b60f2f022c2eec8025255381fb16f1205
James Critchlow <jamesdcritchlow@gmail.com>
2024-01-31 14:05:04 -0700

    added nvim tree still need to add shortcuts for it

--- lua/james/plugins/init.lua
+++ lua/james/plugins/init.lua
@@ -2,7 +2,10 @@ return {
 	require("james.plugins.colorscheme"), 
 	require("james.plugins.telescope"),
 	require("james.plugins.treesitter"),
-	require("james.plugins.autoclosebracket")
+	require("james.plugins.autoclosebracket"),
+	require("james.plugins.nvim-tree"),
+	require("james.plugins.icons"),
 	-- need to install dap 
-	-- nned to install LSP (sugestions)
+	-- need to install LSP (sugestions)
 }
+
