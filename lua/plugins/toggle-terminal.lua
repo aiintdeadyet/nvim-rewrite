a2c7ab3cf0c703248df839825902f098f5e63272
aiin'tdeadyet <jamesdcritchlow@gmail.com>
2024-06-01 23:52:39 -0600

    replaced bookmarks with harpoon, reformated files

--- lua/plugins/toggle-terminal.lua
+++ lua/plugins/toggle-terminal.lua
@@ -1,3 +1,4 @@
+
 return { -- this is going to be a big pain to actualy implement
 	'akinsho/toggleterm.nvim',
 	version = "*",
--- lua/remaps.lua
+++ lua/remaps.lua
@@ -1,21 +1,29 @@
 -- map leader is set in init.lua to prevent an error
 
-
 -- file management
 -- vim.keymap.set("n", "<leader>fv", vim.cmd.Ex, {}) -- opens nvims default file exporer (netRW)
 vim.keymap.set("n", "<leader>fv", ":Neotree toggle<CR>", {}) -- toggle neotree file exporer
 -- other neotree comments are in the neotree pluggins file
 
-
+-- harpoon and telescope do about the same thing so I have put their keymaps together
 -- telescope
 local builtin = require('telescope.builtin')
-local telmarks = require('telescope').extensions.bookmarks
+local harpoon = require("harpoon")
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fm', telmarks.list, {})
-
+vim.keymap.set('n', '<leader>fnd', "<cmd>Telescope undo<cr>") -- I think i will be finding a nother pluggin for this
+-- harpoon
+vim.keymap.set("n", "<leader>fa", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>fl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>f1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>f2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>f3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>f4", function() harpoon:list():select(4) end)
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<leader>fn", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>fN", function() harpoon:list():next() end)
 
 -- dap (I am smart enough to know there is a better way to call these functions but not smart enough to us it)
 local dap = require("dap")
@@ -37,17 +45,11 @@ vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, {})
 vim.keymap.set('n', '<leader>B', ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>") -- i don't like the way these lines are written
 vim.keymap.set('n', '<leader>lp', ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>") -- this line needs changed to
 vim.keymap.set('n', "<leader>dr", dap.repl.toggle, {})
-
-
 -- dap ui
 vim.keymap.set('n', '<leader>db', ui.toggle)
 -- vim.keymap.set('n', '<leader>db', ":lua require('dapui').toggle()<CR>")
 
-
 -- comment.nvim 
 -- this stuff is in the autocomment.lua file
 
-
 -- toggleterm, remapings have changed to ctrl-t for both and moved to the togglerterm pluggin file
--- vim.keymap.set('n', '<leader>t', ":ToggleTerm<CR>", opts)
--- vim.keymap.set('t', '<esc>', "<C-t>", opts)
